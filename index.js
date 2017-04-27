1a650d9223b07e6698523b8154d2dbdc9f18889e
eseom <me@eseom.org>
2017-04-28 01:33:58 +0900

    map route object from another plugins

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const register = (server, options, next) => {
-	const makeRoutes = (prefix = '') => {
-		const methods = ['get', 'post', 'put', 'del', 'any']
-		const innerRoute = (routeOptions) => server.route(routeOptions)
-		methods.forEach((hm) => {
-			let method = hm.toUpperCase()
-			if (hm === 'any') method = '*'
-			if (hm === 'del') method = 'delete'
-			innerRoute[hm] = (path, config, handler) => {
-        return server.route({
-  				path: `${prefix}${path}`,
-  				method,
-  				handler,
-  				config,
-  			})
-  		}
+  const origRoute = server.root.route
+  const makeRoutes = (prefix = '') => {
+    const methods = ['get', 'post', 'put', 'del', 'any']
+    const innerRoute = (routeOptions) => origRoute(routeOptions)
+
+    Object.keys(origRoute).forEach((k) => {
+      innerRoute[k] = origRoute[k];
+    });
+
+    methods.forEach((hm) => {
+      let method = hm.toUpperCase()
+      if (hm === 'any') method = '*'
+      if (hm === 'del') method = 'delete' 
+      innerRoute[hm] = (path, config, handler) => {
+        return origRoute({      
+          path: `${prefix}${path}`,     
+          method,                       
+          handler,                      
+          config,                       
+        })                      
+      }          
     })
-		innerRoute.nested = (prefixNested) => makeRoutes(prefix + prefixNested)
-		return innerRoute
-	}
-	server.root.route = makeRoutes()
-	next()
+    innerRoute.nested = (prefixNested) => makeRoutes(prefix + prefixNested)
+    return innerRoute
+  }     
+  server.root.route = makeRoutes()
+  next()
 }
 
 register.attributes = {
-	name: 'hapi-nested-route',
+  name: 'hapi-nested-route',
 }
 
 module.exports = {
-	register,
+  register,
 }
+
